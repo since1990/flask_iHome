d026385c6466625767071420e3167a51d8737659
since1990 <353622522@qq.com>
2019-03-21 22:35:44 +0800

    数据表构造

--- ihome/__init__.py
+++ ihome/__init__.py
@@ -4,7 +4,8 @@ from config import config_map
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 from flask_wtf import CSRFProtect
-from ihome import api_1_0
+import logging
+from logging.handlers import RotatingFileHandler
 import redis
 
 # 数据库
@@ -12,6 +13,19 @@ db = SQLAlchemy()
 
 # redis链接对象
 redis_store = None
+
+# 配置日志信息
+# 设置日志的记录等级
+
+# 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024*1024*100, backupCount=10)
+# 创建日志记录的格式                 日志等级    输入日志信息的文件名 行数    日志信息
+formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+# 为刚创建的日志记录器设置日志记录格式
+file_log_handler.setFormatter(formatter)
+# 为全局的日志工具对象（flask app使用的）添加日记录器
+logging.getLogger().addHandler(file_log_handler)
+logging.basicConfig(level=logging.DEBUG)  # 调试debug级
 
 
 # 工厂模式
@@ -38,6 +52,7 @@ def create_app(config_name):
     global redis_store
     redis_store = redis.StrictRedis(host=config_class.REDIS_HOST, port=config_class.REDIS_PORT)
     # 注册蓝图
+    from ihome import api_1_0
     app.register_blueprint(api_1_0.api, url_prefix="/api/v1.0")
 
     return app
--- ihome/api_1_0/demo.py
+++ ihome/api_1_0/demo.py
@@ -1,8 +1,17 @@
 # coding:utf-8
 from . import api
+from flask import current_app
 
 
 @api.route("/index")
 def index():
+    # logging.error()   # 记录错误信息
+    # logging.warn()   # 警告
+    # logging.info()   # 信息
+    # logging.debug()   # 调试
+    current_app.logger.error("error info")
+    current_app.logger.warn("warn info")
+    current_app.logger.info("info info")
+    current_app.logger.debug("debug info")
     return "index page"
 
