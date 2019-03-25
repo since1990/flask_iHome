c65cc1d61df80c91e332fc283dcb2ee18bd2a1be
since1990 <353622522@qq.com>
2019-03-25 21:40:21 +0800

    增加短信验证码功能

--- ihome/api_1_0/verify_code.py
+++ ihome/api_1_0/verify_code.py
@@ -3,7 +3,7 @@ from . import api
 from ihome.utils.captcha.captcha import captcha
 from ihome import redis_store
 from ihome import constants
-from flask import current_app, jsonify, make_response
+from flask import current_app, jsonify, make_response, request
 from ihome.utils.response_code import RET
 
 
@@ -25,9 +25,38 @@ def get_image_code(image_code_id):
         # 获取异常，记录到日志文件
         current_app.logger.error(e)
         # 返回json数据类型
-        return jsonify(errno=RET.DBERR, errmsg="save image code failed!")
+        return jsonify(errno=RET.DBERR, errmsg=u"保存图片验证码失败")
 
     # 返回图片数据
     resp = make_response(image_data)
     resp.headers["Content-Type"] = "image/jpg"
     return resp
+
+
+@api.route("/sms_codes/<re(r'1[34578]\d{9}'):mobile>")
+def get_sms_code(moblie):
+    """获取短信验证码"""
+    image_code = request.args.get("image_code")
+    image_code_id = request.args.get("image_code_id")
+
+    # 校验参数
+    if not all([image_code, image_code_id]):
+        # 参数不完整
+        return jsonify(errno=RET.PARAMERR, errmsg=u"参数不完整")
+
+    # 取出图片验证码的真实文本
+    try:
+        real_image_code = redis_store.get("image_code_%s" % image_code_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg=u"redis数据库异常")
+
+    # 判断图片验证码是否过期
+    if real_image_code is None:
+        # 表示验证码已过期
+        return jsonify(errno=RET.NODATA, errmsg=u"图片验证码失效")
+
+    # 与用户填写值对比
+    if real_image_code.lower() != image_code.lower():
+        # 数据不想等
+        return jsonify(errno=RET.DATAERR, errmsg=u"数据错误")
---
+++ ihome/libs/yuntongxun/SendTemplateSMS.py
@@ -0,0 +1,58 @@
+# coding=utf-8
+
+
+from CCPRestSDK import REST
+
+#主帐号
+accountSid= '8aaf0708697b6beb0169b27a97121fc2'
+
+#主帐号Token
+accountToken= 'fa03e2adb9ce49b4a420d474b0052c5d'
+
+#应用Id
+appId='8aaf0708697b6beb0169b27a97701fc9'
+
+#请求地址，格式如下，不需要写http://
+serverIP='app.cloopen.com'
+
+#请求端口 
+serverPort='8883'
+
+#REST版本号
+softVersion='2013-12-26'
+
+  # 发送模板短信
+  # @param to 手机号码
+  # @param datas 内容数据 格式为数组 例如：{'12','34'}，如不需替换请填 ''
+  # @param $tempId 模板Id
+
+
+class CCP(object):
+    """自己封装的发送短信的辅助类"""
+    instance = None
+
+    def __new__(cls, *args, **kwargs):
+        # 单例模式
+        if cls.instance is None:
+            obj = super(CCP, cls).__new__(cls)
+            # 初始化REST SDK
+            obj.rest = REST(serverIP, serverPort, softVersion)
+            obj.rest.setAccount(accountSid, accountToken)
+            obj.rest.setAppId(appId)
+
+            cls.instance = obj
+        return cls.instance
+
+    def send_template_sms(self, to, datas, temp_id):
+        result = self.rest.sendTemplateSMS(to, datas, temp_id)
+        for k, v in result.iteritems():
+            if k == 'templateSMS':
+                for k, s in v.iteritems():
+                    print '%s:%s' % (k, s)
+            else:
+                print '%s:%s' % (k, v)
+
+
+if __name__ == '__main__':
+    ccp = CCP()
+    ccp.send_template_sms("13601968924", ["1234", "3"], 1)
